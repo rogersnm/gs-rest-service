9af29bcdfbebc72636ee048b272c3641d03ecf29
Janakra11 <janakrajkhatal@gmail.com>
2018-10-25 15:27:35 +0530

    Creating Pojo class for returning json
    Creating Pojo class for returning json

---
+++ src/main/java/hello/JsonWord.java
@@ -0,0 +1,36 @@
+public class JsonWord
+{
+   private String word;
+   private boolean palindrome;
+   private boolean anagramOfPalindrome;
+   
+   public String getWord()
+   {
+      retrun word;
+   }
+   
+   public void setWord(String word)
+   {
+      this.word = word;
+   }
+    
+   public boolean getPalindrome()
+   {
+      retrun palindrome;
+   }  
+  
+   public void setPalindrome()
+   {
+     this.palindrome = palindrome; 
+   }
+
+    public boolean getAnagramOfPalindrome()
+   {
+      retrun anagramOfPalindrome;
+   }  
+  
+   public void setAnagramOfPalindrome()
+   {
+     this.anagramOfPalindrome = anagramOfPalindrome; 
+   }
+}
