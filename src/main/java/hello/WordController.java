b6608126eaedc85aa3f36e20b0d93d1d8688beee
Anshul Khanna <anshulkhanna91@gmail.com>
2019-01-09 22:19:46 +0530

    Implementing rest endpoint

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,54 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WordController {
-    // TODO Implement the /words/{word} endpoint
+
+    @GetMapping("words/{word}")
+    public ResposeDTO checkWord(@PathVariable String word){
+        ResposeDTO resposeDTO = new ResposeDTO();
+        resposeDTO.setWord(word);
+        if(isPalindrome(word)){
+            resposeDTO.setAnagramOfPalindrome(true);
+            resposeDTO.setPalindrome(true);
+        }else {
+            resposeDTO.setPalindrome(false);
+            resposeDTO.setAnagramOfPalindrome(isAnagramOfPalindrome(word));
+        }
+        return resposeDTO;
+    }
+
+    private Boolean isPalindrome(String word){
+        String reverse = "";
+        int length = word.length();
+        for (int i = length - 1; i >= 0; i--)
+            reverse = reverse + word.charAt(i);
+
+        if (word.equals(reverse)){
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean isAnagramOfPalindrome(String word){
+        int [] count = new int[256];
+        for( int i = 0; i < word.length(); i++ )
+        {
+            char ch = word.charAt(i);
+            count[ch-'a']++;
+        }
+        int oddOccur = 0;
+        for( int cnt:count )
+        {
+            if( oddOccur > 1) // more than 1 char should have odd frequency
+                return false;
+            if( cnt%2 == 1 )
+                oddOccur++;
+        }
+        return true;
+
+    }
 }
